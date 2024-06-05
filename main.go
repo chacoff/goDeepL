fba03385dd95080fb14ad55a1dd74bd44c7355e6
Jaime Gomez-Chacoff <jaime.gomez-chacoff@arcelormittal.com>
2024-06-05 18:25:34 +0200

    help usage added

--- main.go
+++ main.go
@@ -11,9 +11,11 @@ import (
 )
 
 type Args struct{
+	Help 		bool		// help mode
 	From		string		// original language
 	To			string		// target language
 	Text		string		// text to translate
+	Mode 		string
 	Verbose		bool		// verbose mode
 }
 
@@ -29,39 +31,84 @@ var (
 
 //init 
 func init(){
-	file, err := os.Open("key.json")
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
-	}
-	defer file.Close()
-
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&config)
-	if err != nil {
-		fmt.Println("Error decoding JSON:", err)
-		return
-	}
+	readJson()
 }
 
 //main
 func main(){
+	flag.BoolVar(&args.Help, "help", false, "usage help")
+	flag.StringVar(&args.Mode, "mode", "key", "show the used key")
 	flag.StringVar(&args.From, "from", "SP", "original language")
 	flag.StringVar(&args.To, "to", "EN", "target language")
 	flag.StringVar(&args.Text, "text", "Hello!", "input text")
 	flag.BoolVar(&args.Verbose, "v", false, "enable verbose mode")
 
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "\nUsage: goDeepL [options] [arguments]\n")
+		fmt.Fprintln(os.Stderr, "\nOptions:")
+		flag.PrintDefaults()
+		fmt.Fprintln(os.Stderr, "\nModes:")
+		fmt.Fprintln(os.Stderr, "  view-key           View the API key")
+		fmt.Fprintln(os.Stderr, "  update-key         Update the API key")
+		fmt.Fprintln(os.Stderr, "  delete-key         Delete the API key")
+		fmt.Fprintln(os.Stderr, "  translate          Translate text (requires -from, -to, and -text)")
+		fmt.Fprintf(os.Stderr, "\nExample: goDeepL -mode translate -From EN -TO RU -text hello world\n")
+	}
+
+	// Parse the flags
 	flag.Parse()
 
-	fmt.Printf("Translating from %s to %s\n", args.From, args.To)
-	
-	translatedText, err := getTranslation()
-	if err !=nil{
-		fmt.Println("Output error:", err)
+	// Display help if the -help flag is set
+	if args.Help {
+		flag.Usage()
 		return
 	}
 
-	fmt.Println(translatedText)
+	// Handle the different modes
+	switch args.Mode {
+		
+	case "view-key":
+		fmt.Println(config.APIKey)
+
+	case "update-key":
+		fmt.Println("Update key functionality is not yet implemented.")
+
+	case "delete-key":
+		fmt.Println("Delete key functionality is not yet implemented.")
+
+	case "translate":
+		// Validate mandatory arguments for translate mode
+		if args.From == "" || args.To == "" || args.Text == "" {
+			fmt.Println("Error: -from, -to, and -text arguments are required for translate mode.")
+			flag.Usage()
+			os.Exit(1)
+		}
+
+		// Implement your translation logic here
+		fmt.Printf("Translating from %s to %s\n", args.From, args.To)
+
+		translatedText, err := getTranslation()
+		if err != nil {
+			fmt.Println("Output error:", err)
+			return
+		}
+		fmt.Println(translatedText)
+
+	default:
+		fmt.Println("Error: unknown mode. Please use -help to see the usage.")
+		flag.Usage()
+		os.Exit(1)
+	}
+		// fmt.Printf("Translating from %s to %s\n", args.From, args.To)
+
+		// translatedText, err := getTranslation()
+		// if err !=nil{
+		// 	fmt.Println("Output error:", err)
+		// 	return
+		// }
+		// fmt.Println(translatedText)
+
 }
 
 //getTranslations make the http request to deepL. you need to have your own key
@@ -144,4 +191,38 @@ func getReponseText(body []byte) string{
 	}
 
 	return translatedText
+}
+
+//readJson reads the api key of the json file
+func readJson(){
+	file, err := os.Open("key.json")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		return
+	}
+}
+
+//writeJson writes the new key
+func writeJson(){
+	file, err := os.Create("key.json")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(config)
+	if err != nil {
+		fmt.Println("Error encoding JSON:", err)
+		return
+	}
 }
